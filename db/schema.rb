8b397ee2e6e38cd7cc6f7b86ea52dc26157302ab
Mariel F <marielbeth@gmail.com>
2017-09-13 15:57:15 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170913154555) do
+ActiveRecord::Schema.define(version: 20170913155347) do
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
+    t.string "name"
     t.text   "content"
   end
 
